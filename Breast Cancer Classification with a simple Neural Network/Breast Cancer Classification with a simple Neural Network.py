c314abf0c4154a52f3f80643eb47c40679a7e505
Allen stivanson christian <12502120601007@adit.ac.in>
2026-06-30 19:38:38 +0530

    Create Breast Cancer Classification with a simple Neural Network.py

---
+++ Breast Cancer Classification with a simple Neural Network/Breast Cancer Classification with a simple Neural Network.py
@@ -0,0 +1,145 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn.datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
+# importing tensorflow and Keras
+import tensorflow as tf
+tf.random.set_seed(3)
+from tensorflow import keras
+
+
+
+# loading the data from sklearn
+breast_cancer_dataset = sklearn.datasets.load_breast_cancer()
+
+print(breast_cancer_dataset)
+
+# loading the data to a data frame
+data_frame = pd.DataFrame(breast_cancer_dataset.data, columns = breast_cancer_dataset.feature_names)
+
+# print the first 5 rows of the dataframe
+data_frame.head()
+
+# adding the 'target' column to the data frame
+data_frame['label'] = breast_cancer_dataset.target
+
+# number of rows and columns in the dataset
+print(data_frame.shape)
+
+# getting some information about the data
+data_frame.info()
+
+# checking for missing values
+data_frame.isnull().sum()
+
+# statistical measures about the data
+data_frame.describe()
+
+# checking the distribution of Target Variable
+data_frame['label'].value_counts()
+
+data_frame.groupby('label').mean()
+
+X = data_frame.drop(columns='label')
+Y = data_frame['label']
+
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=2)
+print(X.shape, X_train.shape, X_test.shape)
+
+scaler = StandardScaler()
+
+X_train_std = scaler.fit_transform(X_train)
+
+X_test_std = scaler.transform(X_test)
+
+
+# setting up the layers of Neural Network
+
+model = keras.models.Sequential([
+        keras.layers.Flatten(input_shape=(30,)),
+        keras.layers.Dense(20, activation='relu'),
+        keras.layers.Dense(2, activation='sigmoid'),
+])
+# compile the neutral network
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+# training the neural network
+
+history = model.fit(X_train, Y_train, validation_split=0.1, epochs=10)
+
+
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+
+plt.legend(['training data', 'validation data'], loc = 'lower right')
+plt.show()
+
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+
+plt.legend(['training data', 'validation data'], loc = 'upper right')
+plt.show()
+
+
+loss, accuracy = model.evaluate(X_test_std, Y_test)
+print(accuracy)
+
+print(X_test_std.shape)
+print(X_test_std[0])
+
+Y_pred = model.predict(X_test_std)
+
+print(Y_pred.shape)
+print(Y_pred[0])
+
+print(X_test_std)
+
+print(Y_pred)
+
+#  argmax function
+
+my_list = [0.25, 0.56]
+
+index_of_max_value = np.argmax(my_list)
+print(my_list)
+print(index_of_max_value)
+
+# converting the prediction probability to class labels
+
+Y_pred_labels = [np.argmax(i) for i in Y_pred]
+print(Y_pred_labels)
+
+
+input_data = (13.54,14.36,87.46,566.3,0.09779,0.08129,0.06664,0.04781,0.1885,0.05766,0.2699,0.7886,2.058,23.56,0.008462,0.0146,0.02387,0.01315,0.0198,0.0023,15.11,19.26,99.7,711.2,0.144,0.1773,0.239,0.1288,0.2977,0.07259)
+
+# change the input_data to a numpy array
+input_data_as_numpy_array = np.asarray(input_data)
+
+# reshape the numpy array as we are predicting for one data point
+input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+
+# standardizing the input data
+input_data_std = scaler.transform(input_data_reshaped)
+
+prediction = model.predict(input_data_std)
+print(prediction)
+
+prediction_label = [np.argmax(prediction)]
+print(prediction_label)
+
+if prediction_label[0] == 0:
+  print('The tumor is Malignant')
+
+else:
+  print('The tumor is Benign')
